46234c204ea4c0c6cef1baaa9de4bd715745c052
Faget <dudefrombelarus@gmail.com>
2019-12-09 00:05:02 +0300

    Первая миссия сделана

--- Motherbase/scrpts.js
+++ Motherbase/scrpts.js
@@ -1,2 +1,11 @@
-/*hello, ivan*/
+/*hello, ivan,,,,,,,,,,,,,,,,,*/
+
+/*1.1 Объявляем переменные и константы и выводим их в консоли*/
+
+let faget = 'ivan';
+
+const DROBYSH = 'gorshok';
+console.log(DROBYSH, 'and', faget, 'are having fun');
+
+
 
